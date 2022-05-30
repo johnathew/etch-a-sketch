cecdfc84c5e2b97180b0e0e33c0f5887f3640948
johnathew <jakornegay01@gmail.com>
2022-05-30 16:11:38 -0500

    added slider to specify grid size

--- sketch.js
+++ sketch.js
@@ -1,18 +1,54 @@
-const cdiv = document.querySelector(".container");
+const DEFAULT_SIZE = 16;
+let currentSize = DEFAULT_SIZE;
 
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("class", `grid-item`);
+const grid = document.getElementById("grid");
+const sizeSlider = document.getElementById("sizeSlider");
+const sizeValue = document.getElementById("sizeValue")
 
-  cdiv.appendChild(div);
-  hoverColor();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+function changeSize(value) {
+  setCurrentSize(value);
+  updateSizeValue(value);
+  reloadGrid();
 }
 
-function hoverColor() {
-  let gridSquares = document.querySelectorAll(".grid-item");
-  gridSquares.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-      item.style.backgroundColor = "red";
-    });
-  });
+function updateSizeValue(value) {
+  sizeValue.innerHTML = `${value} * ${value}`;
 }
+
+function setCurrentSize(newValue) {
+  currentSize = newValue;
+}
+
+function reloadGrid() {
+  clearGrid();
+  setupGrid(currentSize);
+}
+
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+function setupGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("grid-element");
+
+    gridElement.addEventListener("mouseover", changeColor);
+    gridElement.addEventListener("mousedown", changeColor);
+    grid.appendChild(gridElement);
+  }
+}
+
+function changeColor(e) {
+  e.target.style.backgroundColor = "green";
+}
+
+window.onload = () => {
+  setupGrid(DEFAULT_SIZE);
+};
